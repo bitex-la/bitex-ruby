5efd855edb9a8533361177cc370efe33a1b295a4
Nubis <nubis@woobiz.com.ar>
2014-11-29 13:36:52 -0300

    added trades and account summary endpoints

--- lib/bitex/transaction.rb
+++ lib/bitex/transaction.rb
@@ -2,13 +2,13 @@ module Bitex
   # Utility class for fetching an heterogeneous list of objects that
   # compose your transaction history.
   class Transaction 
-    # @return [Array<Bitex::Buy, Bitex::Sell, Bitex::SpecieDeposit,
+    # @return [Array<Bitex::Bid, Bitex::Ask, Bitex::SpecieDeposit,
     #   Bitex::SpecieWithdrawal, Bitex::UsdDeposit, Bitex::UsdWithdrawal>]
     #   Returns an heterogeneous array with all your transactions for the past
-    #   30 days sorted by descending date.
-    # @see https://bitex.la/developers#user-transactions
+    #   15 days sorted by descending date.
+    # @see https://bitex.la/developers#user-account-summary
     def self.all
-      Api.private(:GET, '/private/transactions').collect{|o| Api.deserialize(o) }
+      Api.private(:GET, '/private/account_summary').collect{|o| Api.deserialize(o) }
     end
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -5,12 +5,12 @@ describe Bitex::Transaction do
     Bitex.api_key = 'valid_api_key'
   end
   
-  it 'gets all transactions' do
-    stub_private(:get, "/private/transactions", 'user_transactions')
+  it 'gets account summary' do
+    stub_private(:get, "/private/account_summary", 'account_summary')
     buy, sell, specie_deposit, specie_withdrawal, usd_deposit, usd_withdrawal =
       Bitex::Transaction.all
-    buy.should be_a Bitex::Buy
-    sell.should be_a Bitex::Sell
+    buy.should be_a Bitex::Bid
+    sell.should be_a Bitex::Ask
     specie_deposit.should be_a Bitex::SpecieDeposit
     specie_withdrawal.should be_a Bitex::SpecieWithdrawal
     usd_deposit.should be_a Bitex::UsdDeposit
