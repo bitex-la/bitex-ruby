02f9fe3242ee48892569ef445bc0368214c02264
Juan Szymczak <juanangelszymczak@gmail.com>
2018-04-18 11:47:18 -0300

    shared_examples_for 'API class with a orderbook'

--- spec/support/from_json_shared_examples.rb
+++ spec/support/from_json_shared_examples.rb
@@ -1,7 +1,7 @@
 shared_examples_for 'API class' do
   it 'stores the unique id' do
-    as_json[1] = 12345678
-    subject.class.from_json(as_json).id.should == 12345678
+    as_json[1] = 12_345_678
+    subject.class.from_json(as_json).id.should == 12_345_678
   end
 
   it 'makes creation times into Time' do
@@ -9,15 +9,15 @@ shared_examples_for 'API class' do
   end
 end
 
-shared_examples_for 'API class with a specie' do
-  it 'makes specie 1 into btc' do
+shared_examples_for 'API class with a orderbook' do
+  it 'makes orderbook 1 into btc_usd' do
     as_json[3] = 1
-    subject.class.from_json(as_json).specie.should == :btc
+    subject.class.from_json(as_json).orderbook.should == :btc_usd
   end
 
-  it 'makes specie 2 into ltc' do
-    as_json[3] = 2
-    subject.class.from_json(as_json).specie.should == :ltc
+  it 'makes specie 5 into btc_ars' do
+    as_json[3] = 5
+    subject.class.from_json(as_json).orderbook.should == :btc_ars
   end
 end
 
@@ -32,35 +32,26 @@ shared_examples_for 'JSON deserializable match' do
 end
 
 shared_examples_for 'JSON deserializable order' do
-  it "sets price as BigDecimal" do
+  it 'sets price as BigDecimal' do
     thing = subject.class.from_json(as_json).price
     thing.should be_a BigDecimal
-    thing.should == 1000.0
+    thing.should == 1_000.0
   end
-  
-  { 1 => :received,
-    2 => :executing,
-    3 => :cancelling,
-    4 => :cancelled,
-    5 => :completed,
-  }.each do |code, symbol|
+
+  { 1 => :received, 2 => :executing, 3 => :cancelling, 4 => :cancelled, 5 => :completed }.each do |code, symbol|
     it "sets status #{code} to #{symbol}" do
       as_json[7] = code
       subject.class.from_json(as_json).status.should == symbol
     end
   end
 
-  { 0 => :not_cancelled,
-    1 => :not_enough_funds,
-    2 => :user_cancelled,
-    3 => :system_cancelled,
-  }.each do |code, symbol|
+  { 0 => :not_cancelled, 1 => :not_enough_funds, 2 => :user_cancelled, 3 => :system_cancelled }.each do |code, symbol|
     it "sets reason #{code} to #{symbol}" do
       as_json[8] = code
       subject.class.from_json(as_json).reason.should == symbol
     end
   end
-  
+
   it 'sets the issuer' do
     as_json[10] = 'User#22'
     subject.class.from_json(as_json).issuer.should == 'User#22'
