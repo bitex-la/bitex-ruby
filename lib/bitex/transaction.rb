9d581d0a41ef9494f8d8b0b4a089b75ad7989066
Juan Szymczak <juanangelszymczak@gmail.com>
2018-04-25 13:59:38 -0300

    a little changes

--- lib/bitex/transaction.rb
+++ lib/bitex/transaction.rb
@@ -1,14 +1,13 @@
 module Bitex
   # Utility class for fetching an heterogeneous list of objects that
   # compose your transaction history.
-  class Transaction 
-    # @return [Array<Bitex::Bid, Bitex::Ask, Bitex::SpecieDeposit,
-    #   Bitex::SpecieWithdrawal, Bitex::UsdDeposit, Bitex::UsdWithdrawal>]
-    #   Returns an heterogeneous array with all your transactions for the past
-    #   15 days sorted by descending date.
+  class Transaction
+    # @return
+    #   [Array<Bitex::Bid, Bitex::Ask, Bitex::SpecieDeposit, Bitex::SpecieWithdrawal, Bitex::UsdDeposit, Bitex::UsdWithdrawal>]
+    #   Returns an heterogeneous array with all your transactions for the past 15 days sorted by descending date.
     # @see https://bitex.la/developers#user-account-summary
     def self.all
-      Api.private(:GET, '/private/account_summary').collect{|o| Api.deserialize(o) }
+      Api.private(:GET, '/private/account_summary').map { |t| Api.deserialize(t) }
     end
   end
 end
