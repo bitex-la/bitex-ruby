f6b0bb2753a7e62b6b7ec67a0b0483060cc68808
Juan Szymczak <juanangelszymczak@gmail.com>
2018-10-04 13:59:11 -0300

    config vcr + webmock

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,8 +1,9 @@
 require 'vcr'
 
 VCR.configure do |c|
-  c.cassette_library_dir = 'spec/vcr_cassettes'
+  c.allow_http_connections_when_no_cassette = true
   c.hook_into :webmock
+  c.cassette_library_dir = 'spec/fixtures'
   c.configure_rspec_metadata!
-  c.default_cassette_options = { record: :once }
+# c.default_cassette_options = { record: :once }
 end
