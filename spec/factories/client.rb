b0a6b231720af6cd8f4be93f78c0f94a05cce859
Juan Szymczak <juanangelszymczak@gmail.com>
2018-10-08 11:39:04 -0300

    add ticker

--- spec/factories/client.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :client, class: Bitex::Client do
-    api_key     Faker::Lorem.characters(30)
-    sandbox     false
-    debug       false
-    ssl_version nil
-  end
-end
--- spec/ticker_spec.rb
+++ spec/ticker_spec.rb
@@ -1,43 +1,44 @@
 require 'spec_helper'
 
 describe Bitex::JsonApi::Ticker do
+  let(:client) { Bitex::Client.new }
   let(:order_book_code) { :btc_usd }
   let(:resource_name) { described_class.name.demodulize.downcase.pluralize }
 
-  shared_examples_for 'founded ticker' do
-    it { response.should be_a(JsonApiClient::ResultSet) }
+  shared_examples_for 'API responses' do
+    subject { response }
 
-    its(:id) { should eq order_book_code.to_s }
-    its(:type) { should eq resource_name }
-    it { should be_a(described_class) }
+    it { is_expected.to be_a(JsonApiClient::ResultSet) }
+    it { is_expected.to be_any }
   end
 
-  describe '#all' do
-    let(:response) { VCR.use_cassette('ticker') { described_class.all } }
+  describe '.all', vcr: { cassette_name: 'ticker' } do
+    let(:response) { client.tickers.all }
 
     subject { response[0] }
 
-    it_behaves_like 'founded ticker'
+    it_behaves_like 'API responses'
+    it { expect(response.uri.path).to eq("/api/#{resource_name}") }
 
-    it { response.uri.path.should eq "/api/#{resource_name}" }
-    it { response.should be_any }
+    it { is_expected.to be_a(described_class) }
+    its(:id) { is_expected.to eq(order_book_code.to_s) }
+    its(:type) { is_expected.to eq(resource_name.singularize) }
   end
 
   describe '#find' do
-    context 'with invalid order book code' do
-      subject { described_class.find('invalid_order_book_code') }
-
-      it { expect { subject }.to raise_exception(Bitex::UnknownOrderBook) }
-    end
-
-    context 'with valid order book code' do
-      let(:response) { VCR.use_cassette('ticker_for_order_book') { described_class.find(order_book_code) } }
+    context 'with valid order book code', vcr: { cassette_name: 'ticker_for_valid_order_book' } do
+      let(:response) { described_class.find(order_book_code) }
 
       subject { response[0] }
 
-      it_behaves_like 'founded ticker'
+      it_behaves_like 'API responses'
+      it { expect(response.uri.path).to eq("/api/#{resource_name}/#{order_book_code}") }
+    end
 
-      it { response.uri.path.should eq "/api/#{resource_name}/#{order_book_code}" }
+    context 'with invalid order book code' do
+      subject { described_class.find(:invalid_order_book_code) }
+
+      it { expect { subject }.to raise_exception(Bitex::UnknownOrderBook) }
     end
   end
 end
