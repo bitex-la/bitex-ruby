15af2bf177f6f9aea57d698c5354f66af90db00b
Juan Szymczak <juanangelszymczak@gmail.com>
2018-11-05 15:12:24 -0300

    add version, and One Time Password  to headers

--- lib/bitex/json_api/base.rb
+++ lib/bitex/json_api/base.rb
@@ -6,14 +6,22 @@ module Bitex
 
       self.site = 'https://dev.bitex.la:3000/api/'
 
-      def self.private_request
-        with_headers(Authorization: Bitex.api_key) { yield }
+      def self.private_request(otp: nil)
+        with_headers(custom_headers(otp)) { yield }
       end
       def_delegator self, :private_request
 
       def self.find(*args)
         super(*args)[0]
       end
+
+      def self.custom_headers(otp)
+        { Authorization: Bitex.api_key }.tap do |headers|
+          headers.merge!(version: VERSION, 'One-Time-Password': otp) if otp.present?
+        end
+      end
+
+      private_class_method :custom_headers
     end
   end
 end
