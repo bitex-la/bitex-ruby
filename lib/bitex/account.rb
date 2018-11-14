c22f820e53229977c254e8ef422ce9900366cd15
Juan Szymczak <juanangelszymczak@gmail.com>
2018-11-14 16:40:52 -0300

    add account resource

---
+++ lib/bitex/account.rb
@@ -0,0 +1,15 @@
+module Bitex
+  # These endpoints give you access general information about your account such as your balances, movements including deposits,
+  # withdrawals as well as executed bids and asks, and your list of KYC Profiles, a collection of all the people or legal
+  # entities associated to your bitex account in any capacity.
+  #
+  # If you're looking for your individual trades see Your latest Trades.
+  # Most accounts only need one KYC Profile, if you need to create sub-accounts have a look at our Reseller API.
+  class Account < Base
+    # GET /api/accounts
+    #
+    # Get all your accounts.
+    #
+    # @return [Array<Account>]
+  end
+end
---
+++ lib/bitex/user.rb
@@ -0,0 +1,5 @@
+module Bitex
+  # Needed to wrap user resources as on Account
+  class User < Base
+  end
+end
